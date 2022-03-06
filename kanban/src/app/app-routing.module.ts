1fbd31f7820c4eb331584c7f74cafe2c6eddb3c1
Aakash Kumar <kumar.aakash1401@gmail.com>
2022-03-07 01:10:14 +0530

    Created Main view component for showing Kanban board

--- kanban/src/app/app-routing.module.ts
+++ kanban/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainViewComponent } from './pages/main-view/main-view.component';
+
+const routes: Routes = [
+  { path: '', component: MainViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
